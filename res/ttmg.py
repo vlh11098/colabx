7742efa3e28cc9b263e88b6292dcd8bafdc2d565
vlh11098 <46274878+vlh11098@users.noreply.github.com>
2020-05-03 19:45:08 +0700

    Update ttmg.py

--- res/ttmg.py
+++ res/ttmg.py
@@ -361,17 +361,19 @@ def loadingAn(name="cal"):
       elif name == "lds":
           return display(HTML('''<style>.lds-hourglass {  display: inline-block;  position: relative;  width: 34px;  height: 34px;}.lds-hourglass:after {  content: " ";  display: block;  border-radius: 50%;  width: 34px;  height: 34px;  margin: 0px;  box-sizing: border-box;  border: 20px solid #dfc;  border-color: #dfc transparent #dfc transparent;  animation: lds-hourglass 1.2s infinite;}@keyframes lds-hourglass {  0% {    transform: rotate(0);    animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);  }  50% {    transform: rotate(900deg);    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);  }  100% {    transform: rotate(1800deg);  }}</style><div class="lds-hourglass"></div>'''))
 
-def textAn(TEXT, ty='d'):
-      from IPython.display import HTML
+def textAn():
+n_bar = 50
+iterable = range(10)  # for demo purposes
+n_iter = len(iterable)
+for i, item in enumerate(iterable):
+    j = (i + 1) / n_iter
 
-      if ty == 'd':
-            return display(
-            print('Helo...!')
-            )
-      elif ty == 'twg':
-            textcover = str(len(TEXT)*0.55)
-            return display(HTML('''<style>@import url(https://fonts.googleapis.com/css?family=Anonymous+Pro);.line-1{font-family: 'Anonymous Pro', monospace;    position: relative;   border-right: 1px solid;    font-size: 15px;   white-space: nowrap;    overflow: hidden;    }.anim-typewriter{  animation: typewriter 0.4s steps(44) 0.2s 1 normal both,             blinkTextCursor 600ms steps(44) infinite normal;}@keyframes typewriter{  from{width: 0;}  to{width: '''+textcover+'''em;}}@keyframes blinkTextCursor{  from{border-right:2px;}  to{border-right-color: transparent;}}</style><div class="line-1 anim-typewriter">'''+TEXT+'''</div>'''))
+    sys.stdout.write('\r')
+    sys.stdout.write(f"[{'=' * int(n_bar * j):{n_bar}s}] {int(100 * j)}%")
+    sys.stdout.flush()
 
+    sleep(1)  
+ 
 def updateCheck(self, Version):
     class UpdateChecker(object):
 
