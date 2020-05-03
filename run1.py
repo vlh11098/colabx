dd109141149161ce3178db2c5f8966a8a5554595
vlh11098 <46274878+vlh11098@users.noreply.github.com>
2020-05-03 16:10:53 +0700

    Update run1.py

--- run1.py
+++ run1.py
@@ -2,10 +2,10 @@
 import time
 import progressbar
 from time import sleep
-bar = progressbar.ProgressBar(maxval=300, \
+bar = progressbar.ProgressBar(maxval=10, \
     widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 bar.start()
-for i in range(300):
+for i in range(10):
     bar.update(i+1)
     sleep(1)
 bar.finish()
