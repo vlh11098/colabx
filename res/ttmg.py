a6e9d694a1f6bd3f12a2fe2ceccc6174dfbf16ae
vlh11098 <46274878+vlh11098@users.noreply.github.com>
2020-05-03 20:10:18 +0700

    Update ttmg.py

--- res/ttmg.py
+++ res/ttmg.py
@@ -360,11 +360,8 @@ def loadingAn(name="cal"):
           return display(HTML('''<style>.lds-hourglass {  display: inline-block;  position: relative;  width: 34px;  height: 34px;}.lds-hourglass:after {  content: " ";  display: block;  border-radius: 50%;  width: 34px;  height: 34px;  margin: 0px;  box-sizing: border-box;  border: 20px solid #dfc;  border-color: #dfc transparent #dfc transparent;  animation: lds-hourglass 1.2s infinite;}@keyframes lds-hourglass {  0% {    transform: rotate(0);    animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);  }  50% {    transform: rotate(900deg);    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);  }  100% {    transform: rotate(1800deg);  }}</style><div class="lds-hourglass"></div>'''))
 
 def textAn(ty='d'):
- 
-
-
       if ty == 'd':
-            return static format_time(timestamp, precision=datetime.timedelta(seconds=1)) 
+            return (abc)
     
 def updateCheck(self, Version):
     class UpdateChecker(object):
